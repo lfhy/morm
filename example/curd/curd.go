7bcfa25d060a4e1875a6c1f0b80d3e8677046bf4
3000y <821923589@qq.com>
2025-08-15 09:09:30 +0800

    🌱 添加 CURD 示例代码
    添加了 CURD（创建、读取、更新、删除）操作的示例代码，用于演示如何使用 morm 库进行数据库操作。代码包括初始化数据库配置、创建数据、查询数据、更新数据和删除数据的功能，并展示了如何使用日志记录操作结果。
    
    添加 CURD 示例代码，以便更好地展示 morm 库的基本使用方法，帮助开发者快速上手。

---
+++ example/curd/curd.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lfhy/morm"
+)
+
+// 数据库结构体
+// 如果数据在mongo需要按mongo-driver进行标注
+// 在其他gorm的（mysql，sqlite）按gorm进行标注
+type DBSturct struct {
+	ID   string `bson:"_id" gorm:"id"`
+	Name string `bson:"name" gorm:"name"`
+}
+
+// 表名或集合名
+func (DBSturct) TableName() string {
+	return "dbtable"
+}
+
+func (DBSturct) M() morm.ORMModel {
+	return db.Model(&DBSturct{})
+}
+
+var db morm.ORM
+
+func init() {
+	// 初始化配置文件
+	configPath := "/path/to/config.toml"
+	err := morm.InitORMConfig(configPath)
+	if err != nil {
+		fmt.Printf("配置文件加载错误:%v\n", err)
+		panic(err)
+	}
+	// 使用自定义日志:db.SetLogger
+	// 数据库初始化
+	db = morm.Init()
+}
+
+func main() {
+	var db DBSturct
+
+	// 写入数据
+	id, err := db.M().Create(&DBSturct{Name: "test"})
+	if err != nil {
+		fmt.Printf("写入数据失败:%v\n", err)
+		return
+	}
+	fmt.Printf("写入数据成功,id:%s\n", id)
+
+	// 创建查询
+	var find DBSturct
+	err = db.M().Where("id", id).Find().One(&find)
+	if err != nil {
+		fmt.Printf("查询失败:%v\n", err)
+		return
+	}
+	fmt.Printf("查询结果:%+v\n", find)
+
+	// 更新数据
+	err = db.M().Where("id", id).Update(&DBSturct{Name: "test1"})
+	if err != nil {
+		fmt.Printf("更新失败:%v\n", err)
+		return
+	}
+	err = db.M().Where("id", id).Find().One(&find)
+	if err != nil {
+		fmt.Printf("查询失败:%v\n", err)
+		return
+	}
+	fmt.Printf("更新结果:%+v\n", find)
+
+	// 删除数据
+	err = db.M().Where("id", id).Delete()
+	if err != nil {
+		fmt.Printf("删除失败:%v\n", err)
+		return
+	}
+	fmt.Println("删除成功")
+}
